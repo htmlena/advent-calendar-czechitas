cd6746f8c694e30ca0c8cfae4e3f3d307cea8837
ivhart <ivhart77@gmail.com>
2021-11-27 14:08:23 +0100

    new component ActualDate

---
+++ src/Components/ActualDate/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ActualDate = () => {
+  const current = new Date();
+  const date = `${current.getDate()}.${
+    current.getMonth() + 1
+  }.${current.getFullYear()}`;
+
+  return (
+    <div className="ActualDate">
+      <h1>Dnes je: {date}</h1>
+    </div>
+  );
+};
+
+export default ActualDate;
