fad4b1736a994f4a7eb4ba2aef73d6540762eccf
Lenka Shynkarova <lenka.shynkarova@biano.com>
2021-11-27 14:41:41 +0100

    styles

--- src/Components/Advent-Calendar/index.jsx
+++ src/Components/Advent-Calendar/index.jsx
@@ -5,10 +5,10 @@ import Image from 'react-bootstrap/Image'
 const Box = (props) => {
   return (
     <div className="box-main-container">
-      <div>
+      <div className="box-number">
         <p>{props.data.number}</p>
       </div>
-      <div>
+      <div className="box-text">
        <p>{props.data.text}</p>
       </div>
       <Image rounded className="box-image" src={props.data.img} alt={`Adventní okénko číslo ${props.data.number}` }/>
