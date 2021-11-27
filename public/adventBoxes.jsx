aae98674f886e97c15c4ee25fa4ccf6f26935815
Lenka Shynkarova <lenka.shynkarova@biano.com>
2021-11-27 10:48:20 +0100

    array of objects

---
+++ public/adventBoxes.jsx
@@ -0,0 +1,147 @@
+const adventBoxes = [
+    {
+        number: 1,
+        img: "./czechitas-advent-images/1.jpg",
+        text: "",
+        open: false
+      },
+    {
+        number: 2,
+        img: "./czechitas-advent-images/2.jpg",
+        text: "",
+        open: false
+      },
+     {
+        number: 3,
+        img: "./czechitas-advent-images/3.jpg",
+        text: "",
+        open: false
+      },
+      {
+        number: 4,
+        img: "./czechitas-advent-images/4.jpg",
+        text: "",
+        open: false
+      },
+      {
+        number: 5,
+        img: "./czechitas-advent-images/5.jpg",
+        text: "",
+        open: false
+      },
+      {
+        number: 6,
+        img: "./czechitas-advent-images/6.jpg",
+        text: "",
+        open: false
+      },
+      {
+        number: 7,
+        img: "./czechitas-advent-images/7.jpg",
+        text: "",
+        open: false
+      },
+      {
+        number: 8,
+        img: "./czechitas-advent-images/8.jpg",
+        text: "",
+        open: false
+      },
+      {
+        number: 9,
+        img: '/czechitas-advent-images/9.jpg',
+        text: "",
+        open: false
+      },
+      {
+        number: 10,
+        img: '/czechitas-advent-images/10.jpg',
+        text: "",
+        open: false
+      },
+      {
+        number: 11,
+        img: '/czechitas-advent-images/11.jpg',
+        text: "",
+        open: false
+      },
+      {
+        number: 12,
+        img: '/czechitas-advent-images/12.jpg',
+        text: "",
+        open: false
+      },
+      {
+        number: 13,
+        img: '/czechitas-advent-images/13.jpg',
+        text: "",
+        open: false
+      },
+      {
+        number: 14,
+        img: '/czechitas-advent-images/14.jpg',
+        text: "",
+        open: false
+      },
+      {
+        number: 15,
+        img: '/czechitas-advent-images/15.jpg',
+        text: "",
+        open: false
+      },
+      {
+        number: 16,
+        img: '/czechitas-advent-images/16.jpg',
+        text: "",
+        open: false
+      },
+      {
+        number: 17,
+        img: '/czechitas-advent-images/17.jpg',
+        text: "",
+        open: false
+      },
+      {
+        number: 18,
+        img: '/czechitas-advent-images/18.jpg',
+        text: "",
+        open: false
+      },
+      {
+        number: 19,
+        img: '/czechitas-advent-images/19.jpg',
+        text: "",
+        open: false
+      },
+      {
+        number: 20,
+        img: '/czechitas-advent-images/20.jpg',
+        text: "",
+        open: false
+      },
+      {
+        number: 21,
+        img: '/czechitas-advent-images/21.jpg',
+        text: "",
+        open: false
+      },
+      {
+        number: 22,
+        img: '/czechitas-advent-images/22.jpg',
+        text: "",
+        open: false
+      },
+      {
+        number: 23,
+        img: '/czechitas-advent-images/23.jpg',
+        text: "",
+        open: false
+      },
+      {
+        number: 24,
+        img: '/czechitas-advent-images/24.jpg',
+        text: "",
+        open: false
+      },
+]
+
