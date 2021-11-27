2310d570298aedd7538fb4436155e0328db7ee31
Barbora Petříková <bb.petrikova@gmail.com>
2021-11-27 14:16:10 +0100

    grid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import React from 'react';
 import { adventBoxes } from './adventBoxes';
-
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 import Box from './Components/Advent-Calendar';
 
 function App() {
   return (
     <>
+        <Container fluid>
+    <Row>
       {adventBoxes.map((adventBox) => {
         return <Box data={adventBox} key={adventBox.number} />;
       })};
+      </Row>
+      </Container>
     </>
   );
 }
--- src/Components/Advent-Calendar/index.jsx
+++ src/Components/Advent-Calendar/index.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import './style.css'
 import Image from 'react-bootstrap/Image'
 
+import Col from 'react-bootstrap/Col'
+
 const Box = (props) => {
   return (
     <>
+
+        <Col md={2}>
       <p>
         {props.data.number}{props.data.text}
       </p>
       <Image rounded className="box" src={props.data.img} alt={`Adventní okénko číslo ${props.data.number}` }/>
+      </Col>
     </>
   );
 };
