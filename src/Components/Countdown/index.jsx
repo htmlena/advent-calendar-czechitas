7c866272b64cab91bc1d7399eff85026842326eb
ivhart <ivhart77@gmail.com>
2021-11-27 17:10:02 +0100

    countdown ready

--- src/Components/Countdown/index.jsx
+++ src/Components/Countdown/index.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const Countdown = () => {
   const today = newDate();
-  const year = today > (today.getMonth() === 12 && today.getDay === 24) ? today.getFullYear()+1 : today.getFullYear()
-  const christmas = new Date(year, 12, 24)
-  const difference = christmas - today
-  return (
-  difference
-  )}
+  const year =
+    today > (today.getMonth() === 12 && today.getDay === 24)
+      ? today.getFullYear() + 1
+      : today.getFullYear();
+  const christmas = new Date(year, 12, 24);
+  const difference = christmas - today;
 
   return (
-<div className="Countdown">
-      <h1>Do Vanoc zbyva: {christmas}</h1>
+    <div className="Countdown">
+      <h1>Do Vanoc zbyva: {difference}</h1>
     </div>
-  )
-}
+  );
+};
 
 export default Countdown;
